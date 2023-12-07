97694578180dc73f37f7361ff9734afbc32e099f
vanderleipinto <vandecopinto@gmail.com>
2023-12-07 15:46:59 -0300

    Adding a Blog Post Show Action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,4 +3,9 @@ class BlogPostsController < ApplicationController
     @blog_posts = BlogPost.all
   end
 
+  def show
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
 end
