bd4f80fe90a7cda0129c33a318302e378f5e7c82
vanderleipinto <vandecopinto@gmail.com>
2023-12-08 09:25:45 -0300

    doing Adding a New Blog Post Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
 
   # Defines the root path route ("/")
