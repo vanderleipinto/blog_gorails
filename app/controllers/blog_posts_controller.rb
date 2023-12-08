a7f17e9427971416b3424d33ffdfcfa5bbf51413
vanderleipinto <vandecopinto@gmail.com>
2023-12-08 20:23:25 -0300

    doing Edit & Update Blog Post Actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -14,14 +14,16 @@ class BlogPostsController < ApplicationController
   end
 
   def create
-    @blog_post = BlogPost.new(blog_post_params)
-    if @blog_post.save      
+    @blog_post = BlogPost.new(blog_post_params) 
+    if @blog_post.save
       redirect_to @blog_post
-      
-    else
-      redirect_to @blog_post
-      #render :new, status: :unprocessable_entity
+    else      
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @blog_post = BlogPost.find(params[:id])
   end
 
   private 
