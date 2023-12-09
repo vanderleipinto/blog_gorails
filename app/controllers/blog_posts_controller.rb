0dfb68134c802b6d5539bf5fdc487360f82ac8cd
vanderleipinto <vandecopinto@gmail.com>
2023-12-08 23:24:53 -0300

    Finished Adding a Blog Post Destroy Action and Refactoring

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,13 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only:  [:show, :edit, :update, :destroy ]
+  #ou
+  #before_action :set_blog_post, exept: [:index, :new, :create,  ]
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -23,13 +26,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -37,9 +40,22 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    # @blog_post = BlogPost.find(params[:id])
+    @blog_post.destroy  
+    redirect_to root_path
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+    end
+  end
+
   private 
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
-  end
+    
 end
