a6cd0d18d9a3a0b44362b31c2dec9f8453711d74
vanderleipinto <vandecopinto@gmail.com>
2023-12-08 21:12:24 -0300

    Finished Edit & Update Blog Post Actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -24,6 +24,17 @@ class BlogPostsController < ApplicationController
 
   def edit
     @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private 
