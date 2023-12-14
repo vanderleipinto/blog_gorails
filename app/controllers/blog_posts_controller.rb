3ea24791984513ae4671ec55ea11fcc156d880fa
vanderleipinto <vandecopinto@gmail.com>
2023-12-13 22:17:41 -0300

    Doing Scheduled posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -11,7 +11,7 @@ class BlogPostsController < ApplicationController
   
   
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = BlogPost.all *
   end
 
   def show
@@ -53,7 +53,7 @@ class BlogPostsController < ApplicationController
   end
 
   def set_blog_post
-    @blog_post = BlogPost.published.find(params[:id])
+    user_signed_in? ?  BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       redirect_to root_path
     end
@@ -66,6 +66,6 @@ class BlogPostsController < ApplicationController
   private 
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at) 
     
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,9 +4,18 @@ class BlogPost < ApplicationRecord
   validates :body, presence: true
 
   scope :draft, -> { where(published_at: nil)}
-  scope :published, -> { where("published_at <= :", Time.current) }
-  scope :scheduled, -> { where("published_at >= :", Time.current) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ? ", Time.current) }
   
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
   
-  
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
